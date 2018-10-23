c38adbf140fc4cf6ad865a3c233bab80588a7ed8
Richard Yi-Te Lu <yitelu@gmail.com>
2018-10-22 23:10:30 -0700

    Android ListView

---
+++ app/src/main/java/com/ytl/listviewdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.ytl.listviewdemo;
+
+import android.database.DataSetObserver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+
+        final ArrayList<String> myFamily = new ArrayList<String>();
+        myFamily.add("Nick");
+        myFamily.add("Sarah");
+        myFamily.add("John");
+        myFamily.add("Fido");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Log.i("YTL person selected: ", myFamily.get(i));
+                Toast.makeText(MainActivity.this, myFamily.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+    }
+}
